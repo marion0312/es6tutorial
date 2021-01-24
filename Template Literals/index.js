6b0c5e0e396fabb9dcb1fbdd5d281310301154bb
Service Unit <Marion Balba>
2021-01-25 00:33:20 +0800

    Intial commit

---
+++ Template Literals/index.js
@@ -0,0 +1,11 @@
+let word1 = 'Marion';
+let word2 = 'Balba';
+
+let fullName = `${word1} ${word2}`;
+
+// console.log(fullName);
+
+document.getElementById('name').innerText = fullName;
+
+//LOG:
+// Encountered a null in fullName
