410e9c6df8750b45de589498457bec64bb973b79
Service Unit <Marion Balba>
2021-01-25 00:50:38 +0800

    Added the usage of math operations

--- Template Literals/index.js
+++ Template Literals/index.js
@@ -3,9 +3,15 @@ let word2 = 'Balba';
 
 let fullName = `${word1} ${word2}`;
 
+let num1 = 5;
+let num2 = 10;
+
+let sum = `${num1 + num2}`;
+
 // console.log(fullName);
 
 document.getElementById('name').innerText = fullName;
+document.getElementById('sum').innerText = sum;
 
 //LOG:
 // Encountered a null in fullName
